c754440d2c982ddc3db123f8f308a7730da01e67
AnkushMalik <ankushmalik631@gmail.com>
2020-05-24 19:00:26 +0530

    Add conditional collapse to sidenav
    Add .collapse to list children dropdown according to pathname

--- components/listitem.jsx
+++ components/listitem.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { route } from 'next/dist/next-server/server/router';
 
 export const ListItem = ({ title, routeName, children }) => {
     const router = useRouter();
@@ -21,19 +22,25 @@ export const ListItem = ({ title, routeName, children }) => {
         hmbg.classList.toggle('is_active')
     }
 
+    const active_route = router.pathname
+
     return (
         <li className='list_items' onClick={() => !children ? mob_sidenav_handler() : null}>
             {children ?
                 <div className='list_item_with_children'>
                     <Link href={routeName}>
-                        <div onClick={dropdownHandler} className={"list_title " + (router.pathname === routeName ? "active" : "")}>
+                        <div onClick={dropdownHandler} className={"list_title " + (active_route === routeName ? "active" : "")}>
                             <span>{title}</span>
                             <span className='dd_ico'>
                                 <img src={require('../img/dropdown.svg')} />
                             </span>
                         </div>
                     </Link>
-                    <ul className='list_child collapsed' >
+                    <ul className={
+                        'list_child ' +
+                        (active_route.includes(routeName) && active_route.match(/\//g)
+                            .length > 1 ? '' : 'collapsed')
+                    }>
                         {children
                             .sort((a, b) => {
                                 let titleA = a.title;
@@ -50,7 +57,7 @@ export const ListItem = ({ title, routeName, children }) => {
                                 <Link key={i} href={`${routeName}` + `${e.routeName}`}>
                                     <li
                                         onClick={mob_sidenav_handler}
-                                        className={"list_child_items " + (router.pathname === routeName + e.routeName ? "active" : "")}
+                                        className={"list_child_items " + (active_route === routeName + e.routeName ? "active" : "")}
                                     >{e.title}</li>
                                 </Link>
                             )
@@ -59,7 +66,7 @@ export const ListItem = ({ title, routeName, children }) => {
                 </div>
                 :
                 <Link href={routeName}>
-                    <div className={"list_title " + (router.pathname === routeName ? "active" : "")}>
+                    <div className={"list_title " + (active_route === routeName ? "active" : "")}>
                         <span>{title}</span>
                     </div>
                 </Link>
