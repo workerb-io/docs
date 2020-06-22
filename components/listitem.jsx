b33b43be1073d92cd70309e526bc5f1138e916b7
Saurabh Garg <sgarg@workerb.io>
2020-06-21 19:20:56 -0700

    Removed unused import

--- components/listitem.jsx
+++ components/listitem.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { route } from 'next/dist/next-server/server/router';
 
 export const ListItem = ({ title, routeName, children }) => {
     const router = useRouter();
