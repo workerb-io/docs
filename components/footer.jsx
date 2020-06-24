aca2252d10d58bc22c146737e2c92e7e2d18ef0e
AnkushMalik <ankushmalik631@gmail.com>
2020-06-24 15:06:11 +0530

    Add GA events to footer links (fl) and modify helper.js

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,42 +1,44 @@
 import React from 'react'
 import ReactGA from 'react-ga'
 
-export const Footer = () => (
-    <footer className='footer'>
-        <div className="row footer_main_area">
-            <div className="footer_children footer_copyright">
-                <span>© 2020 workerB. All rights reserved.</span>
+export const Footer = () => {
+    return (
+        <footer className='footer'>
+            <div className="row footer_main_area">
+                <div className="footer_children footer_copyright">
+                    <span>© 2020 workerB. All rights reserved.</span>
+                </div>
+                <div className="footer_children tnc">
+                    <a href="https://workerb.io/terms_and_conditions.html">Terms &amp; Condition</a>
+                    <a href="https://workerb.io/privacy_policy.html">Privacy Policy</a>
+                </div>
+                <div className="footer_children social_icons">
+                    <a href="https://github.com/workerb-io/" target="_blank" rel="noopener noreferrer"
+                        onClick={ReactGA.event({ category: 'wbdocs_fl_github', action: 'click', label: 'footer_link_click' })}>
+                        <img src={require('../img/social/github.svg')} alt='github' />
+                    </a>
+                    <a href="https://twitter.com/workerb_io" target="_blank" rel="noopener noreferrer"
+                        onClick={ReactGA.event({ category: 'wbdocs_fl_twitter', action: 'click', label: 'footer_link_click' })}>
+                        <img src={require('../img/social/twitter.svg')} alt='twitter' />
+                    </a>
+                    <a href="https://medium.com/workerb" target="_blank" rel="noopener noreferrer"
+                        onClick={ReactGA.event({ category: 'wbdocs_fl_medium', action: 'click', label: 'footer_link_click' })}>
+                        <img src={require('../img/social/medium.svg')} alt='medium' />
+                    </a>
+                    <a href="https://www.youtube.com/channel/UCguq9sXrm-B3eF7xqRAPnzA" target="_blank" rel="noopener noreferrer"
+                        onClick={ReactGA.event({ category: 'wbdocs_fl_youtube', action: 'click', label: 'footer_link_click' })}>
+                        <img src={require('../img/social/youtube.svg')} alt='youtube' />
+                    </a>
+                    <a href="https://www.linkedin.com/company/workerb/" target="_blank" rel="noopener noreferrer"
+                        onClick={ReactGA.event({ category: 'wbdocs_fl_linkedin', action: 'click', label: 'footer_link_click' })}>
+                        <img src={require('../img/social/linkedin.svg')} alt='linkedin' />
+                    </a>
+                    <a href="https://spectrum.chat/workerb" target="_blank" rel="noopener noreferrer"
+                        onClick={ReactGA.event({ category: 'wbdocs_fl_spectrum', action: 'click', label: 'footer_link_click' })}>
+                        <img src={require('../img/social/spectrum.svg')} alt='spectrum' />
+                    </a>
+                </div>
             </div>
-            <div className="footer_children tnc">
-                <a href="https://workerb.io/terms_and_conditions.html">Terms &amp; Condition</a>
-                <a href="https://workerb.io/privacy_policy.html">Privacy Policy</a>
-            </div>
-            <div class="footer_children social_icons">
-                <a href="https://github.com/workerb-io/" target="_blank" rel="noopener noreferrer"
-                    onClick={ReactGA.ga('send', { hitType: 'event', eventCategory: 'www_fl_github', eventAction: 'click', eventLabel: 'footer_link_click' })}>
-                    <img src={require('../img/social/github.svg')} alt='github' />
-                </a>
-                <a href="https://twitter.com/workerb_io" target="_blank" rel="noopener noreferrer"
-                    onClick={ReactGA.ga('send', { hitType: 'event', eventCategory: 'www_fl_twitter', eventAction: 'click', eventLabel: 'footer_link_click' })}>
-                    <img src={require('../img/social/twitter.svg')} alt='twitter' />
-                </a>
-                <a href="https://medium.com/workerb" target="_blank" rel="noopener noreferrer"
-                    onClick={ReactGA.ga('send', { hitType: 'event', eventCategory: 'www_fl_medium', eventAction: 'click', eventLabel: 'footer_link_click' })}>
-                    <img src={require('../img/social/medium.svg')} alt='medium' />
-                </a>
-                <a href="https://www.youtube.com/channel/UCguq9sXrm-B3eF7xqRAPnzA" target="_blank" rel="noopener noreferrer"
-                    onClick={ReactGA.ga('send', { hitType: 'event', eventCategory: 'www_fl_youtube', eventAction: 'click', eventLabel: 'footer_link_click' })}>
-                    <img src={require('../img/social/youtube.svg')} alt='youtube' />
-                </a>
-                <a href="https://www.linkedin.com/company/workerb/" target="_blank" rel="noopener noreferrer"
-                    onClick={ReactGA.ga('send', { hitType: 'event', eventCategory: 'www_fl_linkedin', eventAction: 'click', eventLabel: 'footer_link_click' })}>
-                    <img src={require('../img/social/linkedin.svg')} alt='linkedin' />
-                </a>
-                <a href="https://spectrum.chat/workerb" target="_blank" rel="noopener noreferrer"
-                    onClick={ReactGA.ga('send', { hitType: 'event', eventCategory: 'www_fl_spectrum', eventAction: 'click', eventLabel: 'footer_link_click' })}>
-                    <img src={require('../img/social/spectrum.svg')} alt='spectrum' />
-                </a>
-            </div>
-        </div>
-    </footer>
-)
+        </footer >
+    )
+}
--- utils/helper.js
+++ utils/helper.js
@@ -1,8 +1,9 @@
-export const ga_anchor_binder = () =>{
-    let anchors = document.querySelectorAll('a');
+export const ga_anchor_binder = () => {
+    //non footer anchor_tags only
+    let anchors = document.querySelectorAll('.navbar a, .container-fluid a');
     for (let i = 0; i < anchors.length; i++) {
-        anchors[i].addEventListener('click',()=>{
-            ga('send',{
+        anchors[i].addEventListener('click', () => {
+            ga('send', {
                 hitType: 'event',
                 eventCategory: `wbdocs_a_click:${anchors[i].innerText}`,
                 eventAction: 'click'
