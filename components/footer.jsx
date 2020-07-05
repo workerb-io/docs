02f5755c847721c80972072b72fe4ac020d5e433
AnkushMalik <ankushmalik631@gmail.com>
2020-07-05 11:35:58 +0530

    Add GA to header

--- components/footer.jsx
+++ components/footer.jsx
@@ -15,13 +15,13 @@ export const Footer = () => {
                             <div className="footer_children tnc">
                                 <a href="https://workerb.io/terms_and_conditions.html"
                                     onClick={() => {
-                                        ReactGA.event({ category: 'docs_click_footer_term&cond', action: 'click', label: 'footer_link_click' })
+                                        ReactGA.event({ category: 'docs_click_footer_term&cond', action: 'click', label: 'docs_footer_link_click' })
                                         logEvent('docs_click_footer_term&cond')
                                     }}>
                                     Terms &amp; Condition</a>
                                 <a href="https://workerb.io/privacy_policy.html"
                                     onClick={() => {
-                                        ReactGA.event({ category: 'docs_click_footer_privacypol', action: 'click', label: 'footer_link_click' })
+                                        ReactGA.event({ category: 'docs_click_footer_privacypol', action: 'click', label: 'docs_footer_link_click' })
                                         logEvent('docs_click_footer_privacypol')
                                     }}>
                                     Privacy Policy</a>
@@ -29,42 +29,42 @@ export const Footer = () => {
                             <div className="footer_children social_icons">
                                 <a href="https://github.com/workerb-io/" target="_blank" rel="noopener noreferrer"
                                     onClick={() => {
-                                        ReactGA.event({ category: 'docs_click_footer_github', action: 'click', label: 'footer_link_click' })
+                                        ReactGA.event({ category: 'docs_click_footer_github', action: 'click', label: 'docs_footer_link_click' })
                                         logEvent('docs_click_footer_github')
                                     }}>
                                     <img src={require('../img/social/github.svg')} alt='github' />
                                 </a>
                                 <a href="https://www.linkedin.com/company/workerb/" target="_blank" rel="noopener noreferrer"
                                     onClick={() => {
-                                        ReactGA.event({ category: 'docs_click_footer_linkedin', action: 'click', label: 'footer_link_click' })
+                                        ReactGA.event({ category: 'docs_click_footer_linkedin', action: 'click', label: 'docs_footer_link_click' })
                                         logEvent('docs_click_footer_linkedin')
                                     }}>
                                     <img src={require('../img/social/linkedin.svg')} alt='linkedin' />
                                 </a>
                                 <a href="https://www.youtube.com/channel/UCguq9sXrm-B3eF7xqRAPnzA" target="_blank" rel="noopener noreferrer"
                                     onClick={() => {
-                                        ReactGA.event({ category: 'docs_click_footer_youtube', action: 'click', label: 'footer_link_click' })
+                                        ReactGA.event({ category: 'docs_click_footer_youtube', action: 'click', label: 'docs_footer_link_click' })
                                         logEvent('docs_click_footer_youtube')
                                     }}>
                                     <img src={require('../img/social/youtube.svg')} alt='youtube' />
                                 </a>
                                 <a href="https://twitter.com/workerb_io" target="_blank" rel="noopener noreferrer"
                                     onClick={() => {
-                                        ReactGA.event({ category: 'docs_click_footer_twitter', action: 'click', label: 'footer_link_click' })
+                                        ReactGA.event({ category: 'docs_click_footer_twitter', action: 'click', label: 'docs_footer_link_click' })
                                         logEvent('docs_click_footer_twitter')
                                     }}>
                                     <img src={require('../img/social/twitter.svg')} alt='twitter' />
                                 </a>
                                 <a href="https://medium.com/workerb" target="_blank" rel="noopener noreferrer"
                                     onClick={() => {
-                                        ReactGA.event({ category: 'docs_click_footer_medium', action: 'click', label: 'footer_link_click' })
+                                        ReactGA.event({ category: 'docs_click_footer_medium', action: 'click', label: 'docs_footer_link_click' })
                                         logEvent('docs_click_footer_medium')
                                     }}>
                                     <img src={require('../img/social/medium.svg')} alt='medium' />
                                 </a>
                                 <a href="https://spectrum.chat/workerb" target="_blank" rel="noopener noreferrer"
                                     onClick={() => {
-                                        ReactGA.event({ category: 'docs_click_footer_spectrum', action: 'click', label: 'footer_link_click' })
+                                        ReactGA.event({ category: 'docs_click_footer_spectrum', action: 'click', label: 'docs_footer_link_click' })
                                         logEvent('docs_click_footer_spectrum')
                                     }}>
                                     <img src={require('../img/social/forum.svg')} alt='spectrum' />
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,5 +1,6 @@
 import getConfig from 'next/config'
 import React from 'react'
+import ReactGA from 'react-ga'
 import { Amplitude } from "@amplitude/react-amplitude";
 
 import PlayIcon from './svg-components/PlayIcon'
@@ -19,19 +20,28 @@ export const Navbar = ({ hamburger_method }) => {
                     <div className="navbar">
                         <button className="menu_toggle" onClick={hamburger_method}>Menu</button>
                         <div className="logo">
-                            <a href="https://workerb.io" onClick={() => logEvent('docs_click_header_www')}>
+                            <a href="https://workerb.io" onClick={() => {
+                                ReactGA.event({ category: 'docs_click_header_www', action: 'click', label: 'docs_header_link_click' })
+                                logEvent('docs_click_header_www')
+                            }}>
                                 <WorkerBFull />
                             </a>
                         </div>
 
                         <span className="buttons-group">
-                            <a className="login-group" href="https://play.workerb.io/" rel="noreferrer" onClick={() => logEvent('docs_click_header_play')}>
+                            <a className="login-group" href="https://play.workerb.io/" rel="noreferrer" onClick={() => {
+                                ReactGA.event({ category: 'docs_click_header_play', action: 'click', label: 'docs_header_link_click' })
+                                logEvent('docs_click_header_play')
+                            }}>
                                 <span className="login-button">
                                     <PlayIcon />
                                 </span>
                                 <span className="login-text">Playground</span>
                             </a>
-                            <a className="login-group" href={FE_URL + "libraries"} rel="noreferrer" onClick={() => logEvent('docs_click_header_libraries')}>
+                            <a className="login-group" href={FE_URL + "libraries"} rel="noreferrer" onClick={() => {
+                                ReactGA.event({ category: 'docs_click_header_libraries', action: 'click', label: 'docs_header_link_click' })
+                                logEvent('docs_click_header_libraries')
+                            }}>
                                 <span className="login-button">
                                     <LibrariesIcon />
                                 </span>
@@ -39,7 +49,10 @@ export const Navbar = ({ hamburger_method }) => {
                                 <span className="login-text">Libraries</span>
                             </a>
 
-                            <a className="login-group" href={FE_URL + "login"} rel="noreferrer" onClick={() => logEvent('docs_click_header_login')}>
+                            <a className="login-group" href={FE_URL + "login"} rel="noreferrer" onClick={() => {
+                                ReactGA.event({ category: 'docs_click_header_login', action: 'click', label: 'docs_header_link_click' })
+                                logEvent('docs_click_header_login')
+                            }}>
                                 <span className="login-button">
                                     <LoginIcon />
                                 </span>
@@ -50,13 +63,22 @@ export const Navbar = ({ hamburger_method }) => {
                         <span className='right_dropdown'>
                             <img className='right_dropdown_trigger' src={require('../img/dropdown_options.svg')} />
                             <div className='right_dropdown_content'>
-                                <a href='https://play.workerb.io/' onClick={() => logEvent('docs_click_header_playground')}>Play</a>
-                                <a href={FE_URL + "libraries"} onClick={() => logEvent('docs_click_header_libraries')}>Libraries</a>
-                                <a href={FE_URL + "login"} onClick={() => logEvent('docs_click_header_login')}>Login</a>
+                                <a href='https://play.workerb.io/' onClick={() => {
+                                    logEvent('mob_docs_click_header_playground')
+                                    ReactGA.event({ category: 'mob_docs_click_header_playground', action: 'click', label: 'docs_header_link_click' })
+                                }}>Play</a>
+                                <a href={FE_URL + "libraries"} onClick={() => {
+                                    ReactGA.event({ category: 'mob_docs_click_header_libraries', action: 'click', label: 'docs_header_link_click' })
+                                    logEvent('mob_docs_click_header_libraries')
+                                }}>Libraries</a>
+                                <a href={FE_URL + "login"} onClick={() => {
+                                    ReactGA.event({ category: 'mob_docs_click_header_login', action: 'click', label: 'docs_header_link_click' })
+                                    logEvent('mob_docs_click_header_login')
+                                }}>Login</a>
                             </div>
                         </span>
                     </div>
             }
-        </Amplitude>
+        </Amplitude >
     )
 }
